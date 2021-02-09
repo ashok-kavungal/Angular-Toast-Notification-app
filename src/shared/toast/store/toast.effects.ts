637a61afd3b88d077fd948d4d071d8e0fa713df3
ashokkavungal5@gmail.com <ashokkavungal5@gmail.com>
2021-02-09 10:00:46 +0100

    dispatch two actions

--- src/shared/toast/store/toast.effects.ts
+++ src/shared/toast/store/toast.effects.ts
@@ -1,34 +1,25 @@
 import { Injectable } from "@angular/core";
 import { Actions, Effect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { of } from "rxjs";
-import { map, withLatestFrom} from "rxjs/operators";
+import { concatMap, map} from "rxjs/operators";
 
 import * as ToastActions from '../store/toast.actions';
 import * as fromApp from '../../../app/store/app.reducer'
-import { Toast } from "../toast.model";
 
 
 
-const handleRemoval = (toast : Toast)=>{
-    console.log('handle removal is called')
-    setTimeout(() => {
-        console.log('executing removal')
-        return new ToastActions.RemovetoastSuccess(toast.createdAt);      
-    }, 5000);
-}
-
-@Injectable()
+ @Injectable()
 export class ToastEffects{
-    @Effect()
-    toastAdd = this.actions$.pipe(
-        ofType(ToastActions.ADD_TOAST_START),
-        map((data : ToastActions.AddtoastStart)=>{
-            const toast = data.payload;
-            handleRemoval(toast);
-            return new ToastActions.AddtoastSuccess(toast)
-        }),
-    )
+
+   @Effect()
+   toastAdd = this.actions$.pipe(
+       ofType( ToastActions.ADD_TOAST_START),
+       map((action : ToastActions.AddtoastStart)=> action.payload),
+       concatMap( toast => [
+        new ToastActions.AddtoastSuccess(toast),
+        new ToastActions.RemovetoastStart(toast.createdAt)
+  ])
+); 
 
     constructor(
         private actions$: Actions,
